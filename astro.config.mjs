6ce563f11d62c2986ec35cc336b0fc8eae762819
Will Jackson <wj@wovenandwoods.com>
2026-05-18 15:23:12 +0100

    Switched to local fonts rather than Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Lato",
+      cssVariable: "--tg-font-lato",
+      fallbacks: ["sans-serf"],
+      options: {
+        variants: [
+          { weight: "300", src: ["./src/assets/fonts/Lato-Light.woff2"] },
+          { weight: "400", src: ["./src/assets/fonts/Lato-Regular.woff2"] },
+          { weight: "700", src: ["./src/assets/fonts/Lato-Bold.woff2"] },
+        ],
+      },
+    },
+  ],
+});
